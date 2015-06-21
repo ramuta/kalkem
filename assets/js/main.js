8311614ea02847cf8b700b60da1fcd426f2256b6
Matej Ramuta <matej.ramuta@gmail.com>
2015-06-21 14:31:51 +0200

    concentration units connected to each other

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,25 @@
 $(document).ready(function() {
+    var concentrationUnit1 = $("#concentrationUnit1");
+    var concentrationUnit2 = $("#concentrationUnit2");
+
+    concentrationUnit1.change(function() {
+        concentrationUnit2.text(concentrationUnit1.val());
+    });
+
     $("#calculateButton").click(function() {
-        totalVol = parseFloat($("#totalVolume").val());
+        var totalVol = parseFloat($("#totalVolume").val());
+        var resultDiv = $("#result");
 
-        comName = $("#componentName").val();
-        stockCon = parseFloat($("#stockCon").val());
-        desiredCon = parseFloat($("#desiredCon").val());
+        var comName = $("#componentName").val();
+        var stockCon = parseFloat($("#stockCon").val());
+        var desiredCon = parseFloat($("#desiredCon").val());
 
-        calc = (totalVol/stockCon) * desiredCon;
+        var calc = (totalVol/stockCon) * desiredCon;
 
         $("#desiredResult").val(calc);
+
+        resultDiv.empty();
+        resultDiv.append("<p>" + comName + " = " + calc + "</p>");
 
         //alert(calc)
     });
